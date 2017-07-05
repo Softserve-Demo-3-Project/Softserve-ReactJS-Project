07223eab68167bc628865130450731deb1785804
XxXtraCookie <petar_g_kolev@abv.bg>
2017-07-05 14:15:55 +0300

    added onChange & onSubmit funcs to reg form

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
+import {IndexLink, Link} from 'react-router'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
-export const PageLayout = ({ children }) => (
+export const PageLayout = ({children}) => (
   <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
+    <h1>Rentify</h1>
+
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+
     {' · '}
+
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+
+    {' · '}
+
+    <Link to='/registration' activeClassName='page-layout__nav-item--active'>Registration</Link>
+
     <div className='page-layout__viewport'>
       {children}
     </div>
   </div>
 )
 PageLayout.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 }
 
 export default PageLayout
--- src/routes/Registration/components/RegistrationForm.js
+++ src/routes/Registration/components/RegistrationForm.js
@@ -12,102 +12,114 @@ class RegistrationForm extends React.Component {
       repeatPassword: ''
     }
 
-    this.onChange = this
-      .onChange
-      .bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
-
-  render() {
-    return (
-      <form>
-
-        <h1>Registration</h1>
-
-        <div className="form-group">
-          <label className="control-label">Name</label>
-          <input
-            value={this.state.name}
-            onChange={this.onChange}
-            type="text"
-            name="name"
-            id="name"
-            placeholder=" * Name"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <label className="control-label">Age</label>
-          <input
-            value={this.state.age}
-            onChange={this.onChange}
-            type="number"
-            name="age"
-            id="age"
-            placeholder=" * Age"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <label className="control-label">E-mail</label>
-          <input
-            value={this.state.email}
-            onChange={this.onChange}
-            type="email"
-            name="email"
-            id="email"
-            placeholder=" * E-mail"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <label className="control-label">Username</label>
-          <input
-            value={this.state.username}
-            onChange={this.onChange}
-            type="text"
-            name="username"
-            id="username"
-            placeholder=" * Username"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <label className="control-label">Password</label>
-          <input
-            value={this.state.password}
-            onChange={this.onChange}
-            type="password"
-            name="password"
-            id="password"
-            placeholder=" * Password"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <label className="control-label">Repeat Password</label>
-          <input
-            value={this.state.repeatPassword}
-            onChange={this.onChange}
-            type="password"
-            name="repeatPassword"
-            id="repeatPassword"
-            placeholder=" * Repeat Password"
-            className="form-control"/>
-        </div>
-
-        <div className="form-group">
-          <button className="btn btn-primary btn-lg">
-            Create account
-          </button>
-        </div>
-
-      </form>
-    );
-  }
 }
 
+onChange(event) {
+  this.setState({ [event.target.name]: event.target.value });
+}
+
+onSubmit(event) {
+  event.preventDefault();
+  console.log(this.state);
+}
+
+render() {
+  return (
+    <form onSubmit={this.onSubmit}>
+
+      <h1>Registration</h1>
+
+      // Name
+      <div className="form-group">
+        <label className="control-label">Name</label>
+        <input
+          value={this.state.name}
+          onChange={this.onChange}
+          type="text"
+          name="name"
+          id="name"
+          placeholder=" * Name"
+          className="form-control"/>
+      </div>
+
+      // Age
+      <div className="form-group">
+        <label className="control-label">Age</label>
+        <input
+          value={this.state.age}
+          onChange={this.onChange}
+          type="number"
+          name="age"
+          id="age"
+          placeholder=" * Age"
+          className="form-control"/>
+      </div>
+
+      // E-mail
+      <div className="form-group">
+        <label className="control-label">E-mail</label>
+        <input
+          value={this.state.email}
+          onChange={this.onChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder=" * E-mail"
+          className="form-control"/>
+      </div>
+
+      // Username
+      <div className="form-group">
+        <label className="control-label">Username</label>
+        <input
+          value={this.state.username}
+          onChange={this.onChange}
+          type="text"
+          name="username"
+          id="username"
+          placeholder=" * Username"
+          className="form-control"/>
+      </div>
+
+      // Password
+      <div className="form-group">
+        <label className="control-label">Password</label>
+        <input
+          value={this.state.password}
+          onChange={this.onChange}
+          type="password"
+          name="password"
+          id="password"
+          placeholder=" * Password"
+          className="form-control"/>
+      </div>
+
+      // Repeat Password
+      <div className="form-group">
+        <label className="control-label">Repeat Password</label>
+        <input
+          value={this.state.repeatPassword}
+          onChange={this.onChange}
+          type="password"
+          name="repeatPassword"
+          id="repeatPassword"
+          placeholder=" * Repeat Password"
+          className="form-control"/>
+      </div>
+
+      // Submit Button
+      <div className="form-group">
+        <button className="btn btn-primary btn-lg">
+          Create account
+        </button>
+      </div>
+
+    </form>
+  );
+}
+}
 export default RegistrationForm;
