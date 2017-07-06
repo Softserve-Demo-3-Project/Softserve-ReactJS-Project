28c40a9b31f1ef125105da6192033d984410a359
thenis <d.stoilov96@gmail.com>
2017-07-06 17:05:29 +0300

    Changed the version of Bootstrap from 4 to 3.3.7. Added templates for advertisements being displayed

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
+import PanelCollapse from './PanelCollapse'
+
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+  <div className='container'>
+    <PanelCollapse />
+    <PanelCollapse/>
   </div>
 )
 
---
+++ src/routes/Home/components/PanelCollapse.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Collapse, SplitButton, MenuItem } from 'react-bootstrap'
+
+export default class PanelCollapse extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    render() {
+        return (
+            <div className='panel panel-default'>
+                <div className='panel-heading'>
+                    <h4 className='panel-title'>This is an ad</h4>
+                    <SplitButton bsStyle={'primary'} title={'test'} onClick={() => this.setState({open: !this.state.open})}>
+                        <MenuItem>Edit</MenuItem>
+                        <MenuItem>Delete</MenuItem>
+                    </SplitButton>
+                </div>
+                <Collapse in={this.state.open}>
+                    <div>
+                        <div className='panel-body'>
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                            Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
+                    </div>
+                </Collapse>
+            </div>
+        );
+    }
+}
